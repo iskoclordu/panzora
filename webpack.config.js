c962d71a5e82146c78e969432b9cc1cf279ad1b1
iskoclordu <abdulkadirlivaoglu@gmail.com>
2023-01-15 21:57:39 +0300

    Create Directories and Configure Linting,Webpack and other modules.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    home: './src/home.js',
+    menu: './src/menu.js',
+    contact: './src/contact.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Panzora',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(otf|woff|woff2|eot|ttf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
